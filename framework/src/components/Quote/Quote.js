0297bea3f3067777856b9699a4701bd640be4953
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2021-02-10 18:59:37 +0100

    Add author to quote

--- framework/src/components/Quote/Quote.js
+++ framework/src/components/Quote/Quote.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Quote = ({ children }) => {
+const Quote = ({ author, children }) => {
   return (
     <div className='px-6 mx-6 py-5 is-size-3 has-text-weight-light has-text-centered'>
-      “{children}”
+      “{children}”{author ? ` - ${author}` : ''}
     </div>
   )
 }
